cd95eed9a30eabb159e999d984b11bbe8611942e
Guy0851 <delcourt.guillaume08@mail.com>
2024-02-18 10:28:00 +0100

    Header fini + logo home

--- lol-stats/src/App.js
+++ lol-stats/src/App.js
@@ -7,6 +7,7 @@ function App() {
     <div className="App">
       <Router>
         <Routes>
+          <Route path='/'element={<Home/>}/>
           <Route path='/home'element={<Home/>}/>
         </Routes>
       </Router>
--- lol-stats/src/pages/Home.jsx
+++ lol-stats/src/pages/Home.jsx
@@ -1,10 +1,17 @@
 import Header from "../components/Header.jsx";
+import logoLol from "../images/logo-lol.webp";
 
 function Home(){
     return(
         <>
             <Header/>
-            
+            <body className="home-body">
+                <img
+                    className='logo-lol'
+                    src={logoLol}
+                    alt="logo de league of legends"
+                />
+            </body>
         </>
     )
 }
