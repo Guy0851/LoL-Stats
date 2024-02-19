3431729c8baf77ecac5499bf4fb310360109ac3c
Guy0851 <delcourt.guillaume08@gmail.com>
2024-02-20 00:36:08 +0100

    Now, for each match, we can see each user that play that match

--- lol-stats/src/pages/Home.jsx
+++ lol-stats/src/pages/Home.jsx
@@ -3,28 +3,40 @@ import Header from "../components/Header.jsx";
 import logoLol from "../images/logo-lol.webp";
 
 let api_key='RGAPI-b4a2437b-52db-4cf5-9f13-8aec75d59fdf'
-let summoner='GuyLeMuscle'
+let summoner='SprintRun08'
 
 
 
 function Home(){
     const [sumsInfos, setSumsInfos] = useState({});
-    const [matches, setMatches] = useState('');
+    const [matchId, setMatchId] = useState([]);
+    const [matchData, setMatchData] = useState([])
 
     useEffect(() => {
         fetch(`https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summoner}?api_key=${api_key}`)
             .then(response => response.json())
             .then(result => {setSumsInfos(result);
-            return fetch(`https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/${result.puuid}/ids?start=0&count=50&api_key=${api_key}`)
+            return fetch(`https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/${result.puuid}/ids?start=0&count=10&api_key=${api_key}`)
             })
             .then(response => response.json())
             .then(otherResult => {
-                setMatches(otherResult);
+                setMatchId(otherResult);
+                const matchRequests = otherResult.map(matchId => {
+                    return fetch(`https://europe.api.riotgames.com/lol/match/v5/matches/${matchId}?api_key=${api_key}`);
+                })
+                return Promise.all(matchRequests);
+            })
+            .then(responses => {
+                // Traiter les réponses de chaque requête pour obtenir les informations des matchs
+                return Promise.all(responses.map(response => response.json()));
+            })
+            .then(datas => {
+                // Mettre à jour l'état avec les informations des matchs
+                setMatchData(datas);
             })
             .catch(error => console.error('Erreur lors de la récupération des informations du summoner:', error));
     }, []); // Les crochets vides signifient que cet effet ne dépend d'aucune valeur et ne doit être exécuté qu'une seule fois
-    console.log(sumsInfos)
-    console.log(matches)
+
     return (
         <>
             <Header />
@@ -42,8 +54,18 @@ function Home(){
                 <div>
                     <h2>Matches:</h2>
                     <ul>
-                        {Object.values(matches).map((match, index) => (
-                            <li key={index}>{match}</li>
+                        {matchId.map((id, index) => (
+                            <li key={index}>
+                                <strong>Match ID: {id} </strong>
+                                <ul>
+                                    {matchData[index]?.info?.participants?.map((participant, indexBis) => (
+                                        <li key={indexBis}>
+                                            Participant n°{indexBis + 1} : {participant.summonerName}
+                                        </li>
+                                    ))}
+                                </ul>
+                                <br/>
+                            </li>
                         ))}
                     </ul>
                 </div>
